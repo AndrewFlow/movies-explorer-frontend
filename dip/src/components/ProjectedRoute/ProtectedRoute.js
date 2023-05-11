c30e7aab60d62087407f9d97ec331574dae1e013
Andrew Fateev <andrewfateev@mail.ru>
2023-05-11 14:50:02 +0300

    fix:Исправлена ошибка, при попытке перейти по защищеному роуту неавторизированному пользователю, его перекидовало в signup. Теперь перекидывает на главную страницу

--- dip/src/components/ProjectedRoute/ProtectedRoute.js
+++ dip/src/components/ProjectedRoute/ProtectedRoute.js
@@ -2,6 +2,6 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children, ...props }) {
-    return props.LogIn ? children : <Navigate replace to="/signin" />;
+    return props.LogIn ? children : <Navigate replace to="/" />;
 }
 export default ProtectedRoute;
